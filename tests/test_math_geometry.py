af451170db8df4d98cffdf384b39a0d452ff2694
Andrew Torgesen <andrew.torgesen@gmail.com>
2021-11-26 23:50:45 -0500

    expand se3 tests

--- tests/test_math_geometry.py
+++ tests/test_math_geometry.py
@@ -29,7 +29,7 @@ class TestSO3:
         rpy = q.toEuler()
         assert np.isclose(roll, rpy[0]) and np.isclose(pitch, rpy[1]) and np.isclose(yaw, rpy[2])
         
-#class TestSE3:
+class TestSE3:
 #    def test_plus_minus(self):
 #        np.random.seed(144440)
 #        T1 = SE3.random()
@@ -37,19 +37,19 @@ class TestSO3:
 #        T2 = T1 + w
 #        w2 = T2 - T1
 #        assert np.allclose(w, w2)
-#    
-#    def test_composition(self):
-#        np.random.seed(144440)
-#        TI = SE3.identity()
-#        T1 = SE3.random()
-#        T2 = T1 * T1.inverse()
-#        assert np.allclose(TI.tq(), T2.tq())
-#        
-#    def test_chart_maps(self):
-#        np.random.seed(144440)
-#        T = SE3.random()
-#        w = np.random.random((6,1))
-#        T2 = SE3.Exp(SE3.Log(T))
-#        assert np.allclose(T.tq(), T2.tq())
-#        w2 = SE3.Log(SE3.Exp(w))
-#        assert np.allclose(w, w2)
+    
+    def test_composition(self):
+        np.random.seed(144440)
+        TI = SE3.identity()
+        T1 = SE3.random()
+        T2 = T1 * T1.inverse()
+        assert np.allclose(TI.array(), T2.array())
+        
+    def test_chart_maps(self):
+        np.random.seed(144440)
+        T = SE3.random()
+        w = np.random.random(6)
+        T2 = SE3.Exp(SE3.Log(T))
+        assert np.allclose(T.array(), T2.array())
+        w2 = SE3.Log(SE3.Exp(w))
+        assert np.allclose(w, w2)
