9d8a670b34fe9c9921d96cdb38549bda4ea35ea7
Andrew Torgesen <andrew.torgesen@gmail.com>
2021-11-28 22:00:55 -0500

    wip build

--- accel/math/pyceres-src/AddToCeres.cmake
+++ accel/math/pyceres-src/AddToCeres.cmake
@@ -3,9 +3,9 @@ include_directories(${PROJECT_SOURCE_DIR}/internal)
 
 set(PYBIND11_CPP_STANDARD -std=c++11)
 add_subdirectory(${PROJECT_SOURCE_DIR}/pyceres-src/pybind11)
-pybind11_add_module(pyceres ${PROJECT_SOURCE_DIR}/pyceres-src/python_bindings/python_module.cpp
-        ${PROJECT_SOURCE_DIR}/pyceres-src/python_bindings/ceres_examples_module.cpp
-        ${PROJECT_SOURCE_DIR}/pyceres-src/python_bindings/custom_cpp_cost_functions.cpp)
+pybind11_add_module(pyceres ${PROJECT_SOURCE_DIR}/pyceres-src/python_module.cpp
+        ${PROJECT_SOURCE_DIR}/pyceres-src/ceres_examples_module.cpp
+        ${PROJECT_SOURCE_DIR}/pyceres-src/custom_cpp_cost_functions.cpp)
 
 target_link_libraries(pyceres INTERFACE manif-geom-cpp PRIVATE ceres)
 
